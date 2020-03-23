7587626c9f12dd2e80e6b2fbcd3d9ae45d110740
shobhitgarvit <57132637+shobhitgarvit@users.noreply.github.com>
2020-03-23 20:49:07 +0530

    medain filter file

---
+++ medain filter.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+from matplotlib import pyplot as plt
+random_matrix_array = np.random.randint(1, 1550, size=(6, 4))
+median = cv2.medianBlur(random_matrix_array,3)
+
+
+plt.imshow(median)
+plt.show()
+print('adddda',random_matrix_array)
+print('---------------------------------------------------------------------------------------------/n')
+print('after using filtering the pixel size is nearly same /n')
+print(median)
